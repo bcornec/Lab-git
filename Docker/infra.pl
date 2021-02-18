0116f199cfe3e0a9b951f15e67e81940f7c4035c
Bruno Cornec <bruno.cornec@hpe.com>
2021-02-18 12:05:50 +0100

    Avoid error messages during deletion for a clean setup

--- Docker/infra.pl
+++ Docker/infra.pl
@@ -14,8 +14,8 @@ exit(-1) if ($res != 0);
 # Launch containers for Lab
 while ($i <= $nbctn) {
 	my $p = sprintf("%02d",$i);
-	system "docker stop dockerlab$p";
-	system "docker rm dockerlab$p";
+	system "docker stop dockerlab$p 2>/dev/null";
+	system "docker rm dockerlab$p 2>/dev/null";
 
 	my $cmd = "docker run --privileged -d --name dockerlab$p -p 25$p:22 -p 80$p:80 dockerlab";
 	print "Launching $cmd\n";
